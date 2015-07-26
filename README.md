6096f268a62f2fcae444fa190d166ca541d2396c
dinever <dinever@live.cn>
2015-07-26 10:15:54 -0400

    Extract parser and generator into seperate module.

---
+++ README.md
@@ -0,0 +1,4 @@
+Kungfu
+---
+
+Kungfu.js is a Javascript compiler designed to solve the callback hell problem and made for fun.
--- kungfu.js
+++ kungfu.js
@@ -1,11 +1,7 @@
-var esprima = require('esprima');
-var escodegen = require('escodegen');
+var parser = require('./lib/parser');
+var generator = require('./lib/generator');
 
 exports.compile = function(code, options) {
-    try {
-        var parsed = esprima.parse(code);
-    } catch (err) {
-        throw new Error(err.message);
-    }
-    return escodegen.generate(parsed);
-}
+    var parsed = parser.parse(code);
+    return generator.generate(parsed);
+};
