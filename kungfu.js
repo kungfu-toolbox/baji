12d96b122a483008623b13470451458854dce3f8
dinever <dinever@live.cn>
2015-07-29 14:20:49 -0400

    [feat] Add transformer, code can work now.

--- kungfu.js
+++ kungfu.js
@@ -1,7 +1,9 @@
 var parser = require('./lib/parser');
 var generator = require('./lib/generator');
+var transformer = require('./lib/transformer');
 
 exports.compile = function(code, options) {
     var parsed = parser.parse(code);
+    parsed = transformer.transform(parsed);
     return generator.generate(parsed);
 };
